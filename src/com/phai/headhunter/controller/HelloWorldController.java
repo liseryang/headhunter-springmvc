32b4c630f0202e748023e7ae60f551955020dd62
fahri.cyber <fahri.cyber@localhost>
2010-06-12 13:18:20 +0700

    init spring mvc

---
+++ src/com/phai/headhunter/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.phai.headhunter.controller;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+
+public class HelloWorldController implements Controller {
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Return View");
+		logger.info("yoi");
+		return new ModelAndView("helloworld.jsp");
+	}
+}
