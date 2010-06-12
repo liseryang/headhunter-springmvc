8b2791718b6177e3c67feb6f8e058e1fe5f05520
fahri.cyber <fahri.cyber@localhost>
2010-06-12 09:46:49 +0700

    init proyek (pake eclipse)

---
+++ src/com/phai/headhunter/model/Pelamar.java
@@ -0,0 +1,12 @@
+package com.phai.headhunter.model;
+
+import java.util.Date;
+
+public class Pelamar {
+	
+	private String nama;
+	private String alamat;
+	private String tempatLahir;
+	private Date tglLahir;
+
+}
